c83a04f4a7bbbde82fa841f31cf81eb81320076f
Dmytro Cherepakhin <dmytrocherepakhin@gmail.com>
2025-12-01 22:36:03 +0200

    base structure created

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва")
+    description = models.TextField(blank=True, verbose_name="Опис")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Ціна")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Кількість")
+    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Зображення")
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def product_list(request):
-    return HttpResponse("<h1>Інтернет-магазин комп'ютерних комплектуючих</h1>")
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
